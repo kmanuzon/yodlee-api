5d99ad13acc53e635b1e01627fe17a6fd7408625
Karl Heinz Manuzon <karl.manuzon@gmail.com>
2016-05-12 19:08:00 -0400

    Added enums.

---
+++ src/Api/Enumerations.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Yodlee\Api;
+
+class Enumerations
+{
+    const CONTAINER_NAME_BANK = 'bank';
+    const CONTAINER_NAME_CREDIT_CARD = 'creditCard';
+
+    const TRANSACTION_BASE_TYPE_CREDIT = 'CREDIT';
+    const TRANSACTION_BASE_TYPE_DEBIT = 'DEBIT';
+}

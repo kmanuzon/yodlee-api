df40b0a447669415d45e1cd3555465b1315a967b
Karl Heinz Manuzon <karl.manuzon@gmail.com>
2016-06-11 12:51:01 -0400

    Tests.

---
+++ tests/CobrandTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use phpunit\framework\TestCase;
+
+class CobrandTest extends TestCase
+{
+    public function testLogin()
+    {
+        $yodlee = new \Yodlee\Api\Factory('');
+
+        $result = $yodlee->cobrand()->login('', '');
+
+        $this->assertInternalType('boolean', $result);
+    }
+    public function testLogout()
+    {
+        $yodlee = new \Yodlee\Api\Factory('');
+
+        $result = $yodlee->cobrand()->logout();
+
+        $this->assertInternalType('boolean', $result);
+    }
+}
